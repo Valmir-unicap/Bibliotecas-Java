3bfb850480cd0c465c6f8d4b51e10ffe75dd50f2
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2024-03-29 17:36:18 -0300

    Conhecendo os métodos da estrutura HashSet

---
+++ HashSet/Formula1.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+
+public class Formula1 {
+    public void adicionar(HashSet<String> equipe, String piloto){
+        if(equipe.isEmpty()) {
+            System.out.println("Equipe vázia! Inserção efetuada!");
+            equipe.add(piloto);
+
+        } else if (equipe.contains(piloto)){
+                System.out.println("Piloto já está na equipe! Inserção não efetuada!");
+
+        }else{
+            equipe.add(piloto);
+            System.out.println("Inserção efetuada!");
+        }
+    }
+
+    public void remover(HashSet<String> equipe, String piloto){
+        if(equipe.isEmpty()){
+            System.out.println("Equipe vázia! Remoção não foi efetuada!");
+
+        } else if (equipe.contains(piloto)) {
+            System.out.println("Remoção efetuada!");
+            equipe.remove(piloto);
+
+        } else {
+            System.out.println("Piloto não está na equipe! Remoção não efetuada!");
+        }
+    }
+
+    public void update(HashSet<String> equipe, String piloto, String novoPiloto){
+        if(equipe.isEmpty()){
+            System.out.println("Equipe vázia! Upadate não foi efetuado!");
+        }
+        if(equipe.contains(piloto)){
+            remover(equipe,piloto);
+            adicionar(equipe,novoPiloto);
+
+        }else{
+            System.out.println("Piloto não encontrado!");
+        }
+    }
+
+    public void exibirTodosPilotos(HashSet<String> equipe){
+        if(equipe.isEmpty()){
+            System.out.println("Equipe vázia!");
+        }else{
+            for (String i : equipe) {
+                System.out.println("- "+ i);
+            }
+        }
+    }
+}
